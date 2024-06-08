b3dfe41624ce08571e0c9b587905c83e38f0bdf4
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-06-08 14:44:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', function () {
+	let inputValue = Number(document.getElementById('ip').value);
+	const output = document.getElementById('output');
+
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(inputValue);
+		}, 2000);
+	})
+	.then(value => {
+		output.textContent = `Result: ${value}`;
+		return new Promise((resolve,reject) => {
+			setTimeout(() => {
+				resolve(value * 2);
+			}, 1000);
+		});
+	})
+	.then(value => {
+		output.textContent = `Result: ${value}`;
+		return new Promise((resolve,reject) => {
+			setTimeout(() => {
+				resolve(value - 3);
+			},1000);
+		});
+	})
+	.then(value => {
+        output.textContent = `Result: ${value}`;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(value / 2);
+            }, 1000);
+        });
+    })
+	.then(value => {
+        output.textContent = `Result: ${value}`;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(value + 10);
+            }, 1000);
+        });
+    })
+	.then(value => {
+        output.textContent = `Final Result: ${value}`;
+    })
+	.catch(error => {
+        output.textContent = `Error: ${error}`;
+    });
+})
